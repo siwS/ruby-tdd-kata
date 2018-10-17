a5f3835fed9a97a21863aeb8c6b9714f891b1e74
sofia <tzi.sof@gmail.com>
2018-10-17 22:57:45 +0100

    Refactor

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,14 +7,9 @@ class StringCalculator
     return 0 if numbers.empty?
     raise "Invalid error"  if comma_followed_by_newline?(numbers)
 
-    long_delimiter = long_delimiter(numbers)
-    if long_delimiter != nil && long_delimiter.size > 0
-      delimiter = long_delimiter[0]
-    else
-      delimiter = parse_delimiter(numbers)
-    end
+    delimiter = parse_delimiter(numbers)
     numbers_array = calculate_numbers_array(delimiter, numbers)
-    numbers_array = numbers_array.map { |x| x.to_i }
+    numbers_array = numbers_array
 
     negative_numbers = numbers_array.select { |x| x < 0 }
     raise "Negative numbers are not allowed: #{negative_numbers.join(" ")}"  if negative_numbers.any?
@@ -24,29 +19,38 @@ class StringCalculator
     return sum
   end
 
+  # Regexes
 
   private def comma_followed_by_newline?(numbers)
     /,\n/.match(numbers)
   end
 
   private def parse_delimiter(numbers)
-    /(?<=\/\/)./.match(numbers)
+    long_delimiter = long_delimiter(numbers)
+    if long_delimiter != nil && long_delimiter.size > 0
+      long_delimiter[0]
+    else
+      /(?<=\/\/)./.match(numbers)
+    end
+  end
+
+  private def long_delimiter(numbers)
+    /(?<=\/\/\[).+?(?=\]\n)/.match(numbers)
   end
 
   private def rest_of_text(numbers)
     /(?<=\/\/\W).*$/.match(numbers).post_match
   end
 
+  # Calculate numbers
+
   private def calculate_numbers_array(delimiter, numbers)
     if delimiter
-      rest_of_text(numbers).split(/[\n#{delimiter}]/)
+      numbers_array = rest_of_text(numbers).split(/[\n#{delimiter}]/)
     else
-      numbers.split(/[\n,]/)
+      numbers_array = numbers.split(/[\n,]/)
     end
-  end
-
-  private def long_delimiter(numbers)
-    /(?<=\/\/\[).+?(?=\]\n)/.match(numbers)
+    numbers_array.map { |x| x.to_i }
   end
 
 end
